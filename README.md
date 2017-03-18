a16e10e8ccec1854db8c7ff4ddd0850f94a66271
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2017-03-18 16:26:06 +0700

    initialize repository

---
+++ README.md
@@ -0,0 +1,17 @@
+## Wonderful Indonesia for Vacations
+
+Wonderful Indonesia! Free template for you with popular destinations from Indonesia.
+
+![Vacations Preview](https://muhibbudins.github.io/vacations/img/preview.png)
+
+- [See Demo](https://muhibbudins.github.io/vacations/)
+- [Downloads ](https://github.com/muhibbudins/vacations/archive/master.zip)
+- [Hire Author](https://www.linkedin.com/in/muhibbudins/)
+
+### Customize & Requirement
+
+You can see [The Skeleton](https://github.com/muhibbudins/template-skeleton) of template [Here](https://github.com/muhibbudins/template-skeleton) .
+
+### License
+
+This project under MIT License
---
+++ js/script.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+	// Vendor Initialize
+	$('[data-aos]').attr('data-aos-once', true);
+	AOS.init();
+
+	// Scroll to Div
+	$('a[data-slide="slide"]').click(function(e) {
+		e.preventDefault();
+		$('.sidebar-overlay').trigger('click');
+		var target = $(this).data('slide-target');
+	    $("html, body").animate({
+	        scrollTop: $(target).offset().top - 100
+	    }, 600);
+	})
+
+	// Dynamic Scrolled Navbar
+	function hasScrolled() {
+		var navbar = $('.navbar');
+		if (window.scrollY > 50) {
+			navbar.addClass('scrolled');
+		} else {
+			navbar.removeClass('scrolled');
+		}
+	}
+
+	$(window).scroll(function() {
+		hasScrolled();
+	});
+
+	$(window).resize(function() {
+		hasScrolled();
+	});
+
+	hasScrolled();
+
+	// Dynamic Sidebar
+	$('.navbar-bars').click(function(e) {
+		e.preventDefault();
+		$('.sidebar').addClass('active');
+		$('.sidebar-overlay').addClass('active');
+	});
+	$('.sidebar-overlay').click(function(e) {
+		e.preventDefault();
+		$('.sidebar').removeClass('active');
+		$('.sidebar-overlay').removeClass('active');
+	});
+
+	// Dynamic Overlay Clones
+	var overlay 	 = $('.overlay_effect'),
+		overlayArray = ['overlay__right','overlay__left','overlay__top','overlay__bottom','overlay__center'];
+
+	overlay.map(function(k, v) {
+		var currents = $(v);
+
+		for (var i in overlayArray) {
+			var nextOverlay = $(v).clone().removeClass('overlay_effect').addClass(overlayArray[i]);
+			currents.after(nextOverlay);
+		}
+
+		currents.remove();
+	});
+
+	// Swiper Initialize
+	new Swiper('#swiper-travel', {
+	    speed: 400,
+	    slidesPerView: 5,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: true,
+		breakpoints: {
+			320: {
+				slidesPerView: 2,
+				spaceBetween: 10
+			},
+			480: {
+				slidesPerView: 3,
+				spaceBetween: 20
+			},
+			640: {
+				slidesPerView: 4,
+				spaceBetween: 60
+			}
+		}
+	});
+});
